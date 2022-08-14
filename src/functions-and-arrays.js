315fda030babe9578b9b5985464402ba5872846d
xavy92 <javaz_15@hotmail.com>
2022-08-14 18:31:59 -0500

    Se completaron ejercicos de funciones

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,90 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+ if(number1 > number2) {
+    return number1
+  } else if(number2 > number1) {
+    return number2
+  } else {
+    return number1
+  }
+}
 
 
 
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+  const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (!words.length) return null;
+    if (words.length === 1) return words[0];
+    let maxcadena = words[0]
+    for (let i = 0; i < words.length; i++){
+      if (words[i].length > maxcadena.length) {
+        maxcadena = words[i]
+        //console.log(maxcadena, words[i])
+            
+      }
+    } return maxcadena
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  if (!numbers.length) return 0;
+  if (numbers.length === 1) return numbers[0]
+  //console.log(!numbers.length, numbers)
+   
+  let sum = 0
+  for (i = 0; i < numbers.length; i++) {
+    if(numbers[i] === 0) {
+      return 0
+    }else{
+      sum += numbers[i];
+    }
+   
+  }
+  //console.log(sum)
+  return sum
+}
+
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(parametros) {
+  //console.log(parametros)
+  if(parametros.length === 0) return 0
+  if (parametros.length === 1) return parametros[0]
+  let result = 0
+  let resultString = 0
+  for (i = 0; i < parametros.length; i++) {
+   
+    if(parametros[i] === 0) {
+      return 0
+    } else if(typeof parametros[i] === 'object') {
+      console.error('this is object')
+      //console.log( typeof parametros[i], 'object')
+    }
+    else if(typeof parametros[i] === 'array') {
+      console.error('this is array')
+    } 
+    
+    else if(typeof parametros[i] === "string" ){
+      //console.log(parametros[i].length, 'ultimo ejercicio')
+      result += parametros[i].length
+      //console.log( result, 'validacion')
+      
+    } else {
+      result += parametros[i]
+    }
+   
+    
+  }
+  return result
+}
 
 
 
@@ -26,16 +92,69 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbersAvg) {
+  if(numbersAvg.length === 0) return null
+
+  let average = 0
+  for (i = 0; i < numbersAvg.length; i++) {
+    
+      average += numbersAvg[i];
+    
+   
+  }
+  return average / numbersAvg.length
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(wordsArr) {
+  if(wordsArr.length === 0) return null
+
+  let promedio = 0
+  for(i = 0; i < wordsArr.length; i++) {
+    promedio += wordsArr[i].length;
+    //console.log(promedio, 'promedio')
+  }
+  //console.log(promedio / wordsArr.length)
+  return promedio / wordsArr.length
+  
+ }
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(arreglo) {
+  // console.log(arreglo, 'estoy aqui')
+  if(arreglo.length === 0){
+    return null
+  }
+  let sumaString = 0
+  let sumaNumber = 0
+  let sumaBoolean = 0 
+  let promedio = 0
+  for(i =0; i < arreglo.length; i++) {
+    
+    if(typeof arreglo[i] === 'string' ){
+      sumaString += arreglo[i].length
+      // console.log(arreglo[i].length, 'string')
+    } else if (typeof arreglo[i] === 'boolean' ){
+      if (arreglo[i] === true ){
+        sumaBoolean += 1
+      }else{
+        sumaBoolean += 0
+      }
+
+     // console.log(arreglo[i], 'boolean')
+    }else {
+      // console.log(arreglo[i], 'numero')
+      sumaNumber += arreglo[i]
+    }
+  }
+  
+  promedio =  sumaString + sumaNumber + sumaBoolean
+  return promedio / arreglo.length
+  // console.log( promedio)
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,14 +171,104 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(prop) {
+  // console.log(prop)
+  if(prop.length === 0){
+    return null
+  }
+  // let uniqified = prop[1]
+  // let uni = prop[0]
+  // for(i = 0; i < prop.length; i++){
+  //   if(uniqified === prop[i]){
+  //     console.log('igual', prop[i], typeof prop[i])
+  //     return [uniqified]
+      
+  //   } else if(uni !== prop[i]) {
+  //     console.log('diferente', prop[i], typeof prop[i])
+  //     return [prop]
+
+  //   }
+      
+  // }
+ 
+  // for(let i =0; i < prop.length; i++) {
+  //   for(let k = i +1; k < prop.length; k++){
+  //     if(prop[i].includes(prop[k])){
+  //       console.log('diferent',prop[i], prop[k] )
+  //       return prop
+       
+  //     } else if(prop[i]=== prop[k]) {
+  //       console.log('igual', [prop[0]], prop[i])
+  //       return [prop[0]]
+  //     }
+  //   }
+  // }
+
+  // let index = prop.indexOf(prop[0 ])
+  // console.log(index)
+  
+  const newArray = []
+  prop.forEach((item)=>{
+    if(!newArray.includes(item)){
+      // console.log(newArray)
+      newArray.push(item);
+    }
+  })
+  // console.log(newArray)
+  return newArray
+
+}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(wordsFind, item) {
+  //console.log(wordsFind, item)
+  if(wordsFind.length === 0){
+    return null
+  }
+
+  // let word = '';
+  // wordsFind.forEach((i)=>{
+  //   // console.log(i, 'prueba', item)
+  //   //  let exist = wordsFind.find(item === i)
+  //   //  console.log(exist, 'exit' )
+  //   if(i === item){
+  //     word = true
+  //     // console.log('iguallllll', true)
+  //     // return true
+  //   } else {
+  //     word = false
+  //     // console.log('es falso')
+  //     // return false
+  //   }
+    
+  // })
+  // console.log(word) 
+
+  let noExist = wordsFind.indexOf(item)
+  //console.log(noExist)
+  if(noExist === -1 ){
+    return false
+  } else {
+    return true
+  }
+
+  // let find = wordsFind.find(item => item === wordsFind[i])
+
+  //   console.log(item)
+  //   if(find){
+  //     return true
+    
+  //   } else {
+  //     return false
+  //   }
+
+  
+  
+}
 
 
 
@@ -78,7 +287,26 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(wordsCount, element) {
+  console.log(wordsCount, element)
+  if(wordsCount.length === 0){
+    return 0
+  }
+
+let repetidos = 0;
+
+wordsCount.forEach(e=>{
+  // console.log(e, 'string numero')
+  //repetidos[numero] = (repetidos[numero] || 0) + 1;
+  if( e === element){
+   repetidos = repetidos +  1  
+  } 
+})
+return repetidos
+
+
+ 
+}
 
 
 
